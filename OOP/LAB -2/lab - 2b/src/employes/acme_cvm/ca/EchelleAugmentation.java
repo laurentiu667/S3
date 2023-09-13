cb36f9504cccd113d0e39de3671b6e9f46520ab9
laurentiu667 <dilionlaur144@gmail.com>
2023-09-13 14:30:29 -0400

    fr et java

--- OOP/LAB -2/lab - 2b/src/employes/acme_cvm/ca/EchelleAugmentation.java
+++ OOP/LAB -2/lab - 2b/src/employes/acme_cvm/ca/EchelleAugmentation.java
@@ -1,4 +1,14 @@
 package employes.acme_cvm.ca;
 
 public class EchelleAugmentation {
+    private double tauxAugNiv1 = 0.02;
+    private double tauxAugNiv2;
+
+    public void setNiveaux(double augNiv1, double augNiv2){
+
+    }
+    public double tauxAugmentationParNiveau (){
+
+        return
+    }
 }
