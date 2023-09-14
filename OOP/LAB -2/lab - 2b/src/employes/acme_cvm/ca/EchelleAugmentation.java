9dea0e8b109980d8cfacb4b1187552eb53b748a7
laurentiu667 <dilionlaur144@gmail.com>
2023-09-13 20:51:54 -0400

    java

--- OOP/LAB -2/lab - 2b/src/employes/acme_cvm/ca/EchelleAugmentation.java
+++ OOP/LAB -2/lab - 2b/src/employes/acme_cvm/ca/EchelleAugmentation.java
@@ -1,14 +1,26 @@
 package employes.acme_cvm.ca;
 
 public class EchelleAugmentation {
-    private double tauxAugNiv1 = 0.02;
+    private double tauxAugNiv1;
     private double tauxAugNiv2;
 
     public void setNiveaux(double augNiv1, double augNiv2){
-
+        this.tauxAugNiv1 = augNiv1;
+        this.tauxAugNiv2 = augNiv2;
     }
-    public double tauxAugmentationParNiveau (){
+    public double tauxAugmentationParNiveau(int niveau){
+        switch (niveau) {
+            case 1:
+                return this.tauxAugNiv1;
+            case 2:
+                return this.tauxAugNiv2;
+            default:
+                return 0;
+        }
+    }
 
-        return
+    public double tauxAugmentationParSalaire(double salaire){
+        EchelleSalariale echelleSalariale = new EchelleSalariale();
+        return tauxAugmentationParNiveau(echelleSalariale.trouveNiveau(salaire));
     }
 }
--- OOP/LAB -2/lab - 2b/src/employes/acme_cvm/ca/EchelleSalariale.java
+++ OOP/LAB -2/lab - 2b/src/employes/acme_cvm/ca/EchelleSalariale.java
@@ -7,37 +7,40 @@ public class EchelleSalariale {
     private double Niv2Min = 50000.00;
     private double Niv2Max = 74999.99;
 
-    public double salaireMaxParNiveau(int niveau){
-        if (niveau >= 1){
-            return this.Niv1Max;
+    public double salaireMaxParNiveau(int niveau) {
+        switch (niveau) {
+            case 1:
+                return Niv1Max;
+            case 2:
+                return Niv2Max;
+            default:
+                return 0;
         }
-        else if (niveau >= 2) {
-            return this.Niv2Max;
-        }
-        return 0;
     }
-
-    public int trouveNiveau (double salaire){
-        if (salaire <= Niv1Max){
+    public int trouveNiveau(double salaire) {
+        if (salaire >= Niv1Min && salaire <= Niv1Max) {
             return 1;
-        }
-        else if (salaire <= Niv2Max){
+        } else if (salaire >= Niv2Min && salaire <= Niv2Max) {
             return 2;
+        } else {
+            return 0;
         }
-        return 0;
     }
 
-    public double salaireMaxParSalaire(double salaire){
-        if (trouveNiveau(salaire) == 1){
-            return Niv1Max;
-        }
-        else if (trouveNiveau(salaire) == 2){
-            return Niv2Max;
-        }
-        return 0;
+    public double salaireMaxParSalaire(double salaire) {
 
+        switch (trouveNiveau(salaire)){
+            case 1:
+                return Niv1Max;
+            case 2:
+                return Niv2Max;
+            default:
+                return 0;
+        }
     }
 
 
 
 }
+
+
