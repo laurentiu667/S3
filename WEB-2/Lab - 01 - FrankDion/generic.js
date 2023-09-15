a51468c5740521b1eeee54901e44f7b424808b1a
laurentiu667 <dilionlaur144@gmail.com>
2023-09-15 12:34:31 -0400

    web

---
+++ WEB-2/Lab - 01 - FrankDion/generic.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", () => {
+    const genericDiv = document.querySelector(".generic"); 
+    
+    let currentTop = 600;
+    let moveUp = true;
+
+    function moveGeneric() {
+        if (moveUp) {
+            currentTop -=1;
+        } else {
+            currentTop += 1;
+        }
+
+        genericDiv.style.top = currentTop + "px";
+
+        if (currentTop <= 0) {
+            moveUp = false;
+        } else if (currentTop >= 600) {
+            moveUp = true;
+        }
+    }
+
+    setInterval(moveGeneric, 30);
+
+    genericDiv.addEventListener('click', () => {
+        moveUp = !moveUp;
+    });
+});
--- WEB-2/Lab - 01 - FrankDion/script.js
+++ WEB-2/Lab - 01 - FrankDion/script.js
@@ -1,5 +1,6 @@
-const validate = () => {
 
+const validate = () => {
+    console.log(23432);
     let nom = document.querySelector("#nompinput").value;
     let motDepasse = document.querySelector("#mdpinput").value;
 
@@ -27,21 +28,19 @@ const validate = () => {
 }
 
 window.addEventListener("load", () => {
-    
-    const videoOnclick = document.querySelector("video")
-    const bodyy = document.querySelector("#booody")
+    const videoOnclick = document.querySelector("video");
+    const bodyy = document.querySelector("#booody");
 
-    
-        videoOnclick.onplay = () => {
-            bodyy.style.backgroundColor = "#555"
+    videoOnclick.onplay = () => {
+        bodyy.style.backgroundColor = "#555";
+    };
 
-        }
-        
-        videoOnclick.onpause = () => {
-            bodyy.style.backgroundColor = "white"
-        }
+    videoOnclick.onpause = () => {
+        bodyy.style.backgroundColor = "white";
+    };
 
-        videoOnclick.onended = () => {
-            window.location.replace = "./generique.html"
-        }
- })
+    videoOnclick.onended = () => {
+        window.location.href = "./generique.html";
+    };
+
+});
